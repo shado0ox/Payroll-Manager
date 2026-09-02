dc3598b33ec981e4e56396f90de40c92bb5b938d
shady nasif <shady.nasif@gmail.com>
2026-09-03 01:55:59 +0300

    Initialize Gregorian and mobile UI normalization

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import App from './App.tsx';
 import { LanguageProvider } from './i18n/LanguageContext';
 import './index.css';
 import { initRuntimeProtection } from './utils/security';
+import { initUiNormalization } from './utils/uiNormalization';
 
 // Initialize Security & Integrity Layer
 initRuntimeProtection();
+initUiNormalization();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
